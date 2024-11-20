1a30394c19c384380dd4ecb8a34b780959e95042
Nav-Kirat <123044417+Nav-Kirat@users.noreply.github.com>
2024-11-19 18:07:18 -0700

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -14,7 +14,7 @@ st.write(
 )
 
 # Power BI Embed Link (replace with your own embed link)
-power_bi_embed_url = "https://app.powerbi.com/reportEmbed?reportId=3ffbcd45-fe80-41e1-aa7b-23adba1df52a&autoAuth=true&ctid=2ba011f1-f50a-44f3-a200-db3ea74e29b7"
+power_bi_embed_url = "https://app.powerbi.com/reportEmbed?reportId=3ffbcd45-fe80-41e1-aa7b-23adba1df52a&autoAuth=true&ctid=2ba011f1-f50a-44f3-a200-db3ea74e29b7%22"
 
 # Embed Power BI using an iframe
 st.components.v1.html(
